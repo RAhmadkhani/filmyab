d93a0fa191d9d3a066b95bb6ac59fb3ff8b41855
Raheleh <rahelehahmadkhani@hotmail.com>
2020-06-29 15:41:07 +0430

    Card,btn, axios added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<CardDeck style ={{margin:"30px"}}>
+    <Card border="success" >
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title  className="text-center" >Card title</Card.Title>
+        <Button variant="outline-info" >More ...</Button>
+      </Card.Body>
+    </Card>
+    <Card border="danger"  >
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title className="text-center" >Card title</Card.Title>
+        <Button variant="outline-info" >More ...</Button>
+      </Card.Body>
+    </Card>
+    <Card border="warning" >
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title  className="text-center" >Card title</Card.Title>
+        <Button variant="outline-info" >More ...</Button>
+      </Card.Body>
+    </Card>
+</CardDeck>
+
     </div>
   );
 }
